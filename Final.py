f905d3237d7f6b2bd2d3d07fbb5b6660569d4817
Thiago Naves <thiagocn1@al.insper.edu.br>
2022-05-12 17:50:23 -0300

    lista de paises

--- Final.py
+++ Final.py
@@ -24,11 +24,7 @@ for c,v in dadosnormalizados.items():
 #sorteando pais
 pais = dev1.sorteia_pais(dadosnormalizados)
 i = 0
-
-
-
-pais = dev1.sorteia_pais(dadosnormalizados)
-i = 0
+lista_paises = []
 
 print(pais)
 
@@ -38,18 +34,22 @@ print('Você tem 20 tentativa(s)')
 while i <= 20:
     tentativas = input('Qual seu palpite?')
     tentativa = tentativas.lower()
-    if tentativa == pais:
-        print ('Você acertou!')
-        break
-    if tentativa not in nl:
-        print ('Pais desconhecido')
-        print ('Você tem {0} tentativa(s)'.format(20-i))
     if tentativa in nl:
-        if tentativa != pais:
+        lista_paises.append(tentativa)
+        if tentativa == pais:
+            print ('Você acertou!')
+            break
+        else:
             i += 1
             print('Você tem {0} tentativa(s)'.format(20-i))
-            if i== 19:
-                print('Você tem 0 tentativa(s)')
+            if i== 20:
                 print('>>> Você perdeu, o país era: {0}'.format(pais))
                 break
+    else:
+        print ('Pais desconhecido')
+        print ('Você tem {0} tentativa(s)'.format(20-i))
+
+    
+    
+    
             
