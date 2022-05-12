4dd25271e81d5347896b8f4e1eec483631c27a05
Thiago Naves <thiagocn1@al.insper.edu.br>
2022-05-12 14:59:12 -0300

    tentativas sem dicas e distancia

--- Final.py
+++ Final.py
@@ -15,15 +15,41 @@ dadoscerto =  json.loads(texto)
 
 dadosnormalizados = dev1.normaliza(dadoscerto)
 
-
+#lista de paises
+nl = []
+for c,v in dadosnormalizados.items():
+    nl.append(c)
 
 
 #sorteando pais
 pais = dev1.sorteia_pais(dadosnormalizados)
+i = 0
 
-for t in range (20):
-    tentativa = input('Qual seu palpite?')
-    tentativa = tentativa.lower
-    if tentativa == 'dica':
-        print()
 
+
+pais = dev1.sorteia_pais(dadosnormalizados)
+i = 0
+
+print(pais)
+
+print('Um país foi escolhido, tente adivinhar!')
+print('Você tem 20 tentativa(s)')
+
+while i <= 20:
+    tentativas = input('Qual seu palpite?')
+    tentativa = tentativas.lower()
+    if tentativa == pais:
+        print ('Você acertou!')
+        break
+    if tentativa not in nl:
+        print ('Pais desconhecido')
+        print ('Você tem {0} tentativa(s)'.format(20-i))
+    if tentativa in nl:
+        if tentativa != pais:
+            i += 1
+            print('Você tem {0} tentativa(s)'.format(20-i))
+            if i== 19:
+                print('Você tem 0 tentativa(s)')
+                print('>>> Você perdeu, o país era: {0}'.format(pais))
+                break
+            
