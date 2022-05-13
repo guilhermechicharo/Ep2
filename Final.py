747039e57b038ed1963686c8f0be889293f447a8
guilhermechicharo <guilhermecff@al.insper.edu.br>
2022-05-13 15:42:43 -0300

    Dica Cores(se não tiver cor consertar)

--- Final.py
+++ Final.py
@@ -28,6 +28,20 @@ lista_paises = []
 tmax = 20
 distancias = []
 
+#variaveis para dicas
+cores = []
+naopalavras = []
+listacores = []
+
+coresbandeira = dadosnormalizados[pais]['bandeira']
+for a in coresbandeira:
+    if coresbandeira[a] > 0:
+        cores.append(a)
+        if 'outras' in cores:
+            cores.remove('outras')
+
+dicas = ['----------------------------------------','1. Cor da bandeira  - custa 4 tentativas','2. Letra da capital - custa 3 tentativas','3. Área             - custa 6 tentativas','4. População        - custa 5 tentativas','5. Continente       - custa 7 tentativas','0. Sem dica','----------------------------------------']
+
 
 print(pais)
 
@@ -49,6 +63,7 @@ while i <= tmax:
             yb = dadosnormalizados[tentativa]['geo']['longitude']
             d = (dev1.haversine(raio_terra,la,ya,lb,yb))
         
+        
 
             distancias = dev1.adiciona_em_ordem(tentativa,d,distancias)
             for f in range(len(distancias)):
@@ -61,6 +76,29 @@ while i <= tmax:
             if i== tmax:
                 print('>>> Você perdeu, o país era: {0}'.format(pais))
                 break
+    if tentativa == 'dica':
+        print('Mercado de dicas')
+        print(('\n').join(dicas))
+        dc = input('Escolha sua opção [0|1|2|3|4|5]:')
+        if dc == '1':
+            tmax -= 4
+            corescolhida = random.choice(cores)
+            cores.remove(corescolhida)
+            listacores.append(corescolhida)
+            print(('\n').join(listacores))
+                
+
+    
+
+     
+
+
+                            
+                    
+
+
+
+
     else:
         print ('Pais desconhecido')
         print ('Você tem {0} tentativa(s)'.format(tmax-i))
@@ -69,3 +107,4 @@ while i <= tmax:
     
     
             
+    
